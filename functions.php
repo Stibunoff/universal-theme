8d276c0ebbe8a88eb313edd034f588d7903a490b
Stibunoff <d.stibunoff@bk.ru>
2021-03-01 21:05:45 +0300

    master

--- functions.php
+++ functions.php
@@ -1,7 +1,17 @@
 <?php
+// Довабление расширенных возможностей
+if ( ! function_exists( 'universal_theme_setup' ) ) :
+   
+    function universal_theme_setup() {
+      // Добавление тега title
+      add_theme_support( 'title-tag' );
+    }
+endif;
+dd_action( 'after_setup_theme', 'universal_theme_setup' );
 
-add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
+// Подключение стилей и скриптов
 function enqueue_universal_style() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
   wp_enqueue_style( 'universal-theme-style', get_template_directory_uri(  ) . '/assets/css/universal-theme.css', 'style', null, null);
 }
+add_action( 'wp_enqueue_scripts', 'enqueue_universal_style' );
