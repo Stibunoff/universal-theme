1daa7f1ba68acf03274acb716d01210c67dae460
Stibunoff <d.stibunoff@bk.ru>
2021-03-03 12:54:07 +0300

    master

--- front-page.php
+++ front-page.php
@@ -3,65 +3,83 @@
   <div class="container">
     <div class="hero">
       <div class="left">
+          <?php
+    // Объявляем глобальную переменную
+global $post;
+
+$myposts = get_posts([ 
+	'numberposts' => 1,
+  
+]);
+// Проверяем, есть ли посты
+if( $myposts ){
+  // Если есть, запускаем цикл
+	foreach( $myposts as $post ){
+		setup_postdata( $post );
+		?>
         <!-- Выводим записи -->
-        <img src="<?php echo get_template_directory_uri() . '/assets/images/image.png' ?>" alt="" class="post-thumb">
-        <a href="#" class="author">
-          <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png' ?>" alt="" class="avatar">
+        <img src="<?php the_post_thumbnail_url() ?>" alt="" class="post-thumb">
+        <?php $author_id = get_the_author_meta('ID');?>
+        <a href="<?php echo get_author_posts_url($author_id) ?>" class="author">
+          <img src="<?php echo get_avatar_url($author_id)?>" alt="" class="avatar">
           <div class="author-bio">
-            <span class="author-name">Имя Автора</span>
+            <span class="author-name"><?php the_author(); ?></span>
             <span class="author-rank">Должность</span>
           </div>
         </a>
         <div class="post-text">
-          <a href="#" class="category-link javascript">JavaScript</a>
-          <h2 class="post-title">Самые крутые функции в javascript для новичка</h2>
-          <a href="#" class="more">Читать далее</a>
+          <?php the_category(); ?>
+          <h2 class="post-title"> <?php the_title(); ?></h2>
+          <a href="<?php echo get_the_permalink(); ?>" class="more">Читать далее</a>
         </div>
+        		<?php 
+            }
+          } else {
+            ?> <p>Постов нет</p> <?php
+          }
+
+          wp_reset_postdata(); // Сбрасываем $post
+          ?>
       </div>
       <!-- /.left -->
       <div class="right">
         <h3 class="recommend">Рекомендуем</h3>
         <ul class="posts-list">
+                 <?php
+                // Объявляем глобальную переменную
+            global $post;
+
+            $myposts = get_posts([ 
+              'numberposts' => 5,
+              'offset' => 1,
+            ]);
+            // Проверяем, есть ли посты
+            if( $myposts ){
+              // Если есть, запускаем цикл
+              foreach( $myposts as $post ){
+                setup_postdata( $post );
+                ?>
           <!-- Выводим записи -->
           <li class="post">
-            <a href="#" class="category-link css">CSS</a>
-            <a class="post-permalink" href="#">
-              <h4 class="post-title">Новые возможности языка стилей</h4>
+            <?php the_category(); ?>
+            <a class="post-permalink" href="<?php echo get_the_permalink(); ?>">
+              <h4 class="post-title"><?php the_title(); ?></h4>
             </a>
           </li>
-          <!-- Выводим записи -->
-          <li class="post">
-            <a href="#" class="category-link web-design">Web Design</a>
-            <a class="post-permalink" href="#">
-              <h4 class="post-title">Где взять реальные проекты для портфолио</h4>
-            </a>
-          </li>
-          <!-- Выводим записи -->
-          <li class="post">
-            <a href="#" class="category-link javascript">JavaScript</a>
-            <a class="post-permalink" href="#">
-              <h4 class="post-title">20+ инструментов для js-разработчика</h4>
-            </a>
-          </li>
-          <!-- Выводим записи -->
-          <li class="post">
-            <a href="#" class="category-link web-design">Web Design</a>
-            <a class="post-permalink" href="#">
-              <h4 class="post-title">Удачные референсы</h4>
-            </a>
-          </li>
-          <!-- Выводим записи -->
-          <li class="post">
-            <a href="#" class="category-link javascript">JavaScript</a>
-            <a class="post-permalink" href="#">
-              <h4 class="post-title">Топ плагинов jQuery</h4>
-            </a>
-          </li>
+          		<?php 
+            }
+          } else {
+            ?> <p>Постов нет</p> <?php
+          }
+
+          wp_reset_postdata(); // Сбрасываем $post
+          ?>
         </ul>
       </div>
       <!-- /.right -->
     </div>
     <!-- /.hero -->
+  
   </div>
   <!-- /.container -->
 </main>
